40136019b332fc24e20fa6a1b1e70f311a7a0d8f
ian <ian@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-05-15 15:02:23 +0000

    * Refs #3164: Included libexec in included things
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.HttpMonitor@22727 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     package_data = { 
          '': ['*.txt'],
          NAME: ['objects/*','skins/*/*','services/*', 'reports/*/*',
-                'modeler/*/*', 'daemons/*', 'lib/*'],
+                'modeler/*/*', 'daemons/*', 'lib/*', 'libexec/*'],
          },  
 
 
