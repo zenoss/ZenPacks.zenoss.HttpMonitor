47bb16eafdeb08fa0856eba1cbb634968eee0b5f
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-03-02 13:40:16 +0000

    * Fixes #4561: Core ZenPack changes to support restricted ZenManager access.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.HttpMonitor@25767 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.HttpMonitor'
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.HttpMonitor']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = '>=2.1.70'
+COMPAT_ZENOSS_VERS = '>=2.3.70'
 PREV_ZENPACK_NAME = 'HttpMonitor'
 # STOP_REPLACEMENTS
 ################################
