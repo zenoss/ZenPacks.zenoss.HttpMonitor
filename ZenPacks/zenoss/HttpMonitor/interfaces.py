d542c8f1ed513a80417c1fe42466c17918c62300
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.HttpMonitor@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/HttpMonitor/interfaces.py
+++ ZenPacks/zenoss/HttpMonitor/interfaces.py
@@ -1,15 +1,13 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2010, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 or (at your
-# option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2010, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
+
 from Products.Zuul.interfaces import IRRDDataSourceInfo
 from Products.Zuul.form import schema
 from Products.Zuul.utils import ZuulMessageFactory as _t
@@ -32,4 +30,3 @@ class IHttpMonitorDataSourceInfo(IRRDDataSourceInfo):
                              vocabulary='httpMonitorRedirectVocabulary', group=_t('HTTP Monitor'))
     proxyAuthUser = schema.TextLine(title=_t(u'Proxy User'), group=_t('Proxy Credentials'))
     proxyAuthPassword = schema.Password(title=_t(u'Proxy Password'), group=_t('Proxy Credentials'))
-
